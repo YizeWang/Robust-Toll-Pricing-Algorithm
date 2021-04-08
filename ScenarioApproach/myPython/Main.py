31f6c1dc6ffbb88ee10edd8dda8c7a1c8bddf69b
Yize Wang <yizwang@student.ethz.ch>
2021-04-08 18:04:36 +0800

    added pigou network example

--- ScenarioApproach/myPython/Main.py
+++ ScenarioApproach/myPython/Main.py
@@ -9,7 +9,7 @@ from ComputeFlow import *
 from ComputeOptimalTollsApproximate import *
 
 
-verbose = True
+verbose = False
 
 pathCurrFolder = os.path.abspath(os.getcwd())
 pathParFolder = os.path.abspath(os.path.join(pathCurrFolder, '..'))
@@ -28,8 +28,9 @@ nameNet3 = 'Brasse'
 nameNet4 = 'SiouxFallsSmall'
 nameNet5 = 'Friedrichshain'
 nameNet6 = 'Massachusetts'
+nameNet7 = 'Pigou'
 
-nameNet = nameNet2
+nameNet = nameNet7
 numSmpl = 0
 
 with open(pathLogFile, 'wt') as logFile:
@@ -37,23 +38,19 @@ with open(pathLogFile, 'wt') as logFile:
     sys.stdout = logFile
 
     G = ParseTNTP(pathDataFolder, nameNet)
-    # G = ModifyODs(G, numODs=0, scaleFactor=0.853)
     G = ModifyODs(G, numODs=0, scaleFactor=1)
 
     sampleODs = G.dataOD
-    # sampleODs = GenerateSamples(G.dataOD, numSmpl, range=0.05)
     
-    # xNash, costNash, idxZeroFlowNash, idxNonZeroFlowNash, idxUsedNash, flowNash = ComputeFlow(G, G.dataOD, type='Nash', verbose=verbose)
-    # print("Nash Flow Cost: %f" % costNash)
+    xNash, costNash, idxZeroFlowNash, idxNonZeroFlowNash, idxUsedNash, flowNash = ComputeFlow(G, G.dataOD, type='Nash', verbose=verbose)
+    print("Nash Flow Cost: %f" % costNash)
 
-    # xOpt, costOpt, idxZeroFlowOpt, idxNonZeroFlowOpt, idxUsedOpt, flowOpt = ComputeFlow(G, G.dataOD, type='Optimal', verbose=False)
-    # print("Optimal Flow Cost: %f" % costOpt)
+    xOpt, costOpt, idxZeroFlowOpt, idxNonZeroFlowOpt, idxUsedOpt, flowOpt = ComputeFlow(G, G.dataOD, type='Optimal', verbose=verbose)
+    print("Optimal Flow Cost: %f" % costOpt)
 
-    # tOpt = []
-    tOpt = ComputeOptimalTolls(G, sampleODs, pathSolFile, verbose=True)
-    xToll, costToll, _, _, _, flowNash = ComputeFlow(G, sampleODs, tOpt, verbose=True)
+    tOpt = ComputeOptimalTolls(G, sampleODs, pathSolFile, verbose=verbose)
+    xToll, costToll, _, _, _, flowNash = ComputeFlow(G, sampleODs, tOpt, verbose=verbose)
     print("costToll: %f" % costToll)
-    print("tOpt: ", tOpt)
 
     logFile.close()
 
