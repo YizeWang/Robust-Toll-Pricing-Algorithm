b2b10188b6e6a35e9639fe1f075b15dcca8ac9f9
YizeWang <EldenWang@hotmail.com>
2021-05-02 23:04:34 +0800

    plotted poa against num of subsamples

--- ScenarioApproach/myPython/FrankWolfe/FigurePlotter.py
+++ ScenarioApproach/myPython/FrankWolfe/FigurePlotter.py
@@ -28,3 +28,30 @@ class FigurePlotter:
         ax.set_xlabel("Number of Iteration")
         ax.set_ylabel("Price of Anarchy")
         plt.show()
+
+    def PlotConfidence(self):
+        numSubsamples = []
+        PoAs = []
+
+        with open(join(self.__pathFolder, 'Subsample.txt'), 'r') as f:
+            txt = f.readlines()
+            txt = [line.strip('{}\n') for line in txt]
+            reader = csv.reader(txt, quoting=csv.QUOTE_NONNUMERIC)
+            subsampleLists = list(reader)
+        for subsamples in subsampleLists:
+            numSubsamples.append(len(subsamples))
+
+        with open(join(self.__pathFolder, 'PoAsOfMultiStart.csv'), 'r') as f:
+            reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+            PoALists = list(reader)
+        for PoAList in PoALists:
+            PoAs.append(PoAList[-1])
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        ax.scatter(PoAs, numSubsamples)
+
+        ax.set_xlabel("PoA")
+        ax.set_ylabel("Number of Subsamples")
+        plt.show()
