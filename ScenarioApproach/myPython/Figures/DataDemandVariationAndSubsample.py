34e8a5019fbb5ffc1ba4a8e0f1e32a99955a7d02
YizeWang <EldenWang@hotmail.com>
2021-05-11 16:21:48 +0800

    modified figure of robustness and performance

--- ScenarioApproach/myPython/Figures/DataDemandVariationAndSubsample.py
+++ ScenarioApproach/myPython/Figures/DataDemandVariationAndSubsample.py
@@ -15,6 +15,10 @@ popt1List = np.zeros((0, 3))
 popt2List = np.zeros((0, 3))
 demandVariations = [0.01, 0.02, 0.05, 0.10, 0.20]
 
+xlim = [1, 15]
+ylim = [1.0, 1.06]
+xRange = np.linspace(1, 15, 100)
+
 for i, demandVariation in enumerate(demandVariations):
     pathCurrDataFolder = os.path.join(pathDataFolder, "DemandVariation{:.2f}".format(demandVariation))
     pathDataPoA = os.path.join(pathCurrDataFolder, "PoAsOfMultiStart.csv")
@@ -33,7 +37,7 @@ for i, demandVariation in enumerate(demandVariations):
 
     lastPoAs = [PoAList[-1] for PoAList in PoALists]
     numSubsampleList = [len(subsampleList) for subsampleList in subsampleLists]
-    xRange = np.linspace(min(numSubsampleList), max(numSubsampleList), 100)
+    
 
     def f1(x, a, b, c):
         return b/(np.array(x)+a)+c
@@ -46,26 +50,32 @@ for i, demandVariation in enumerate(demandVariations):
     popt2List = np.vstack((popt2List, np.array(popt2)))
 
     plt.figure()
-    plt.scatter(numSubsampleList, lastPoAs, s=2)
+    plt.scatter(numSubsampleList, lastPoAs, s=4)
     plt.plot(xRange, f1(xRange, *popt1), label=r'$y_1=\frac{b}{x+a}+c$', color='r')
     plt.plot(xRange, f2(xRange, *popt2), label=r'$y_2=ae^{-bx}+c$', color='g')
-    plt.title('Performance and Robustness (Variation: {}%)'.format(int(demandVariation*100)))
+    plt.title('Performance and Robustness (Variation: {}%)'.format(int(demandVariation*100)), fontweight="bold")
     plt.xlabel('Support Subsample Cardinality')
     plt.ylabel('Price of Anarchy')
+    plt.xlim(xlim)
+    plt.ylim(ylim)
     plt.legend()
 
 plt.figure()
 for i in range(5): plt.plot(xRange, f1(xRange, *popt1List[i, :]), label='Demand Variation: {}%'.format(int(demandVariations[i]*100)))
-plt.title("Demand Variation's Impact")
+plt.title(r"Demand Variation's Impact ($y=\frac{b}{x+a}+c$)", fontweight="bold")
 plt.xlabel('Support Subsample Cardinality')
 plt.ylabel('Price of Anarchy')
+plt.xlim(xlim)
+plt.ylim(ylim)
 plt.legend()
 
 plt.figure()
 for i in range(5): plt.plot(xRange, f2(xRange, *popt2List[i, :]), label='Demand Variation: {}%'.format(int(demandVariations[i]*100)))
-plt.title("Demand Variation's Impact")
+plt.title(r"Demand Variation's Impact ($y=ae^{-bx}+c$)", fontweight="bold")
 plt.xlabel('Support Subsample Cardinality')
 plt.ylabel('Price of Anarchy')
+plt.xlim(xlim)
+plt.ylim(ylim)
 plt.legend()
 
 plt.show()
