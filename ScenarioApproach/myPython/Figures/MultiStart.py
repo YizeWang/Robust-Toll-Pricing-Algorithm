8284490fa583d91dee6c8e602f6c65f184dc770c
YizeWang <EldenWang@hotmail.com>
2021-05-21 16:54:51 +0800

    modified figure of multistart, added figure of step search comparison

--- ScenarioApproach/myPython/Figures/MultiStart.py
+++ ScenarioApproach/myPython/Figures/MultiStart.py
@@ -6,10 +6,9 @@ import matplotlib.pyplot as plt
 
 
 pathCurrFolder = os.path.abspath(os.getcwd())
-pathDataFolder1 = join(join(join(pathCurrFolder, "Figures"), "DataMultiStart", "WithStepSearch"))
-pathDataFolder2 = join(join(join(pathCurrFolder, "Figures"), "DataMultiStart", "WithoutStepSearch"))
-pathDataPoA1 = join(pathDataFolder1, "PoAsOfMultiStart.csv")
-pathDataPoA2 = join(pathDataFolder2, "PoAsOfMultiStart.csv")
+pathDataFolder = join(join(pathCurrFolder, "Figures"), "DataMultiStart")
+pathDataPoA1 = join(pathDataFolder, "PoAsOfMultiStartwithoutSearch.csv")
+pathDataPoA2 = join(pathDataFolder, "PoAsOfMultiStartwithSearch.csv")
 
 with open(pathDataPoA1, 'r') as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
@@ -21,18 +20,33 @@ with open(pathDataPoA2, 'r') as f:
 
 fig1, ax1 = plt.subplots()
 for PoAList in PoALists1:
-    ax1.plot(range(len(PoAList)), PoAList)
-plt.xlim([0, 50])
-plt.ylim([1.0, 1.1])
+    ax1.plot(range(len(PoAList)), PoAList, linewidth=0.5)
+plt.xlim([0, 30])
+plt.ylim([1.02, 1.05])
 plt.xlabel("Number of Iterations")
 plt.ylabel("Price of Anarchy")
 
 fig2, ax2 = plt.subplots()
 for PoAList in PoALists2:
-    ax2.plot(range(len(PoAList)), PoAList)
-plt.xlim([0, 50])
-plt.ylim([1.0, 1.1])
+    ax2.plot(range(len(PoAList)), PoAList, linewidth=0.5)
+plt.xlim([0, 30])
+plt.ylim([1.02, 1.05])
 plt.xlabel("Number of Iterations")
 plt.ylabel("Price of Anarchy")
 
+numCurve = 0
+colors = ['r', 'b', 'g', 'orange']
+fig3, ax3 = plt.subplots()
+for i in range(len(PoALists1)):
+    if PoALists1[i] == PoALists2[i]: continue
+    if numCurve != 2:
+        ax3.plot(range(len(PoALists1[i])), PoALists1[i], color=colors[numCurve], label='Init Toll {} with Step Search'.format(min(2, numCurve)))
+        ax3.plot(range(len(PoALists2[i])), PoALists2[i], color=colors[numCurve], label='Init Toll {} without Step Search'.format(min(2, numCurve)), linestyle='dashed' )
+    numCurve = numCurve + 1
+    if numCurve > 3: break
+plt.xlim([0, 30])
+plt.ylim([1.02, 1.05])
+plt.xlabel("Number of Iterations")
+plt.ylabel("Price of Anarchy")
+plt.legend(prop={'size': 8})
 plt.show()
