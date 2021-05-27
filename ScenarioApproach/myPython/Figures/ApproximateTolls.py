7a5b8bec0878ca7a50b5d940691dd63b3b001bf5
YizeWang <EldenWang@hotmail.com>
2021-05-27 15:19:23 +0800

    added figure of structure fixing - brasse network

--- ScenarioApproach/myPython/Figures/ApproximateTolls.py
+++ ScenarioApproach/myPython/Figures/ApproximateTolls.py
@@ -6,14 +6,16 @@ from os.path import join
 import matplotlib.pyplot as plt
 
 
+networks = ['SiouxFalls', 'Brasse']
+network = networks[0]
 xRange = np.linspace(0.1, 2.5, num=100, endpoint=True)
 
 pathCurrFolder = os.path.abspath(os.getcwd())
 pathDataFolder = os.path.join(os.path.join(pathCurrFolder, "Figures"), "DataApproximateTolls")
 
-pathDataUE = os.path.join(pathDataFolder, 'nashCosts.csv')
-pathDataSO = os.path.join(pathDataFolder, 'optCosts.csv')
-pathDataToll = os.path.join(pathDataFolder, 'tollCosts.csv')
+pathDataUE = os.path.join(pathDataFolder, 'nashCosts'+network+'.csv')
+pathDataSO = os.path.join(pathDataFolder, 'optCosts'+network+'.csv')
+pathDataToll = os.path.join(pathDataFolder, 'tollCosts'+network+'.csv')
 
 CostUE = np.genfromtxt(pathDataUE, delimiter=',')
 CostSO = np.genfromtxt(pathDataSO, delimiter=',')
